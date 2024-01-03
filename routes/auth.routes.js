502d720e0a95f103696c4ed0765f654a2f85d525
IbroTao <ibrahimtaofeek005@gmail.com>
2024-01-03 22:00:40 +0100

    Setting up user auth controller, route & model cont'd

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -7,12 +7,13 @@ const {
   deleteUser,
   updateUser,
 } = require("../controllers/user.controller");
+const { authMiddleware } = require("../middlewares/authMiddleware");
 const router = Router();
 
 router.post("/register", createUser);
 router.post("/login", loginUser);
 router.get("/all", getAllUsers);
-router.get("/:id", getSingleUser);
+router.get("/:id", authMiddleware, getSingleUser);
 router.put("/:id", updateUser);
 router.delete("/:id", deleteUser);
 
