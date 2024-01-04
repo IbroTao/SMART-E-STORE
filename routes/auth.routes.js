bdd3fb2b24b2545ea8f955d38856195cc14ea174
IbroTao <ibrahimtaofeek005@gmail.com>
2024-01-04 20:53:11 +0100

    Setting up user auth controller, route & model cont'd

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -9,6 +9,7 @@ const {
   blockUser,
   unblockUser,
   handleRefreshToken,
+  logoutUser,
 } = require("../controllers/user.controller");
 const { authMiddleware, isAdmin } = require("../middlewares/authMiddleware");
 const router = Router();
@@ -17,6 +18,7 @@ router.post("/register", createUser);
 router.post("/login", loginUser);
 router.get("/all", authMiddleware, isAdmin, getAllUsers);
 router.get("/refresh", handleRefreshToken);
+router.get("/logout", logoutUser);
 router.get("/:id", authMiddleware, isAdmin, getSingleUser);
 router.put("/edit", authMiddleware, isAdmin, updateUser);
 router.put("/block/:id", authMiddleware, isAdmin, blockUser);
