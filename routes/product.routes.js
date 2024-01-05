5acdb16cc8d189e3c24b15f6510a8a0409aabd91
IbroTao <ibrahimtaofeek005@gmail.com>
2024-01-05 10:33:54 +0100

    Product model, route & controller set up cont'd

--- routes/product.routes.js
+++ routes/product.routes.js
@@ -1,8 +1,14 @@
 const { Router } = require("express");
 const router = Router();
 
-const { createProduct } = require("../controllers/product.controller");
+const {
+  createProduct,
+  getProduct,
+  getProducts,
+} = require("../controllers/product.controller");
 
 router.post("/create", createProduct);
+router.get("/", getProducts);
+router.get("/:id", getProduct);
 
 module.exports = router;
