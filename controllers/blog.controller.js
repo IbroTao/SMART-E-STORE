80971606e24be338f0e7e414c62a55097cd5a0aa
IbroTao <ibrahimtaofeek005@gmail.com>
2024-01-09 19:27:09 +0100

    Blog controller, model & route set up cont'd

--- controllers/blog.controller.js
+++ controllers/blog.controller.js
@@ -39,11 +39,35 @@ const getSingleBlog = asyncHandler(async (req, res) => {
 
 const getAllBlogs = asyncHandler(async (req, res) => {
   try {
+    const blogs = await Blog.find().sort({
+        createdAt: "desc"
+    });
+    if(!blogs) return res.status(404).json({message: "No blog found!"})
+
+    res.status(200).json({ blogs: blogs});
   } catch (error) {
     throw new Error(error);
   }
 });
 
+const deleteBlog = asyncHandler(async(req, res) => {
+    const {id} = req.params;
+    validateMongodbId(id);
+    try{
+        const blog = await Blog.findByIdAndDelete(id);
+        if(!blog) return res.status(404).json({ message: "Blog not found!"});
+
+        res.status(200).json({message: "Blog deleted sucessfully!"});
+    }catch(error){
+        throw new Error(error);
+    }
+})
+
 module.exports = {
   createBlog,
+  updateBlog,
+  deleteBlog,
+  getSingleBlog,
+  getAllBlogs,
+  deleteBlog
 };
--- routes/blog.routes.js
+++ routes/blog.routes.js
@@ -1,8 +1,18 @@
 const { Router } = require("express");
-const { createBlog } = require("../controllers/blog.controller");
+const {
+  createBlog,
+  getAllBlogs,
+  getSingleBlog,
+  updateBlog,
+  deleteBlog,
+} = require("../controllers/blog.controller");
 const { authMiddleware, isAdmin } = require("../middlewares/authMiddleware");
 const router = Router();
 
+router.get("/", authMiddleware, isAdmin, getAllBlogs);
+router.get("/:id", authMiddleware, isAdmin, getSingleBlog);
 router.post("/create", authMiddleware, isAdmin, createBlog);
+router.put("/:id", authMiddleware, isAdmin, updateBlog);
+router.delete("/:id", authMiddleware, isAdmin, deleteBlog);
 
 module.exports = router;
