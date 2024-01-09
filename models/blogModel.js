f5cd450831ba3c713d50b52913cbbcb26dc8453d
IbroTao <ibrahimtaofeek005@gmail.com>
2024-01-09 12:25:05 +0100

    Blog model set up cont'd

--- models/blogModel.js
+++ models/blogModel.js
@@ -1,28 +1,54 @@
 const mongoose = require("mongoose");
 
-const blogSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
+const blogSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    numViews: {
+      type: Number,
+      default: 0,
+    },
+    isLiked: {
+      type: Boolean,
+      default: false,
+    },
+    isDisliked: {
+      type: Boolean,
+      default: false,
+    },
+    likes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    dislikes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    image: {
+      type: String,
+      default:
+        "https://shutterstock.com/image-photo/bloggingblog-concepts-ideas-white-worktable-600w-1029506242.jpg",
+    },
+    author: {
+      type: String,
+      default: "Admin",
+    },
   },
-  description: {
-    type: String,
-    required: true,
-  },
-  content: {
-    type: String,
-    required: true,
-  },
-  numViews: {
-    type: Number,
-    default: 0,
-  },
-  isLiked: {
-    type: Boolean,
-    default: false,
-  },
-  isDisliked: {
-    type: Boolean,
-    default: false,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
