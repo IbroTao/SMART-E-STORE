4b4fb96cbb396778d6abe51b505191b89b316e6a
IbroTao <ibrahimtaofeek005@gmail.com>
2024-01-09 19:12:25 +0100

    Blog controller, model & route set up cont'd

--- controllers/blog.controller.js
+++ controllers/blog.controller.js
@@ -11,6 +11,39 @@ const createBlog = asyncHandler(async (req, res) => {
   }
 });
 
+const updateBlog = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  validateMongodbId(id);
+  try {
+    const blog = await Blog.findByIdAndUpdate(id, re.body, {
+      new: true,
+    });
+    res.status(200).json({ message: "Blog updated successfully" });
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+const getSingleBlog = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  validateMongodbId(id);
+  try {
+    const blog = await Blog.findById(id);
+    if (!blog) return res.status(404).json({ message: "Blog not found!" });
+
+    res.status(200).json({ blog: blog });
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+const getAllBlogs = asyncHandler(async (req, res) => {
+  try {
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
 module.exports = {
   createBlog,
 };
