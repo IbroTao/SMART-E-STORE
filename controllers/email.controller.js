2ed16bf90c39a8dbe3f6bc3403659b4cb7b1af5e
IbroTao <ibrahimtaofeek005@gmail.com>
2024-01-09 15:53:57 +0100

    Made some changes to user controller cont'd

--- controllers/email.controller.js
+++ controllers/email.controller.js
@@ -1,3 +1,17 @@
 const nodemailer = require("nodemailer");
+require("dotenv").config();
 
-const transporter = nodemailer.createTransport({});
+const sender = process.env.SENDER;
+const emailAccess = process.env.PASS;
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: sender,
+    pass: emailAccess,
+  },
+});
+
+function sendEmail(email, resetToken) {
+  const resetLink = `http://localhost:5000/api/user/reset-password/${resetToken}`;
+}
