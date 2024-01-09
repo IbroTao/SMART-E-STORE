f5613885490673fb8c5c71f46a099d43fe7a452a
IbroTao <ibrahimtaofeek005@gmail.com>
2024-01-09 12:19:43 +0100

    Blog model set up

---
+++ models/blogModel.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const blogSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  numViews: {
+    type: Number,
+    default: 0,
+  },
+  isLiked: {
+    type: Boolean,
+    default: false,
+  },
+  isDisliked: {
+    type: Boolean,
+    default: false,
+  },
+});
