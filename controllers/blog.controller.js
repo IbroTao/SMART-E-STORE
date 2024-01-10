0d6096ea02f45dc3e605fcdea8503ed7e11900af
IbroTao <ibrahimtaofeek005@gmail.com>
2024-01-11 00:54:46 +0100

    Like and Dislike a blog & Increase number of views for each blog that is fetched

--- controllers/blog.controller.js
+++ controllers/blog.controller.js
@@ -2,6 +2,7 @@ const Blog = require("../models/blogModel");
 const asyncHandler = require("express-async-handler");
 const validateMongodbId = require("../utils/validateMongodbId");
 
+// CREATE A BLOG
 const createBlog = asyncHandler(async (req, res) => {
   try {
     const blog = await Blog.create(req.body);
@@ -11,6 +12,7 @@ const createBlog = asyncHandler(async (req, res) => {
   }
 });
 
+// UPDATE A BLOG
 const updateBlog = asyncHandler(async (req, res) => {
   const { id } = req.params;
   validateMongodbId(id);
@@ -24,6 +26,7 @@ const updateBlog = asyncHandler(async (req, res) => {
   }
 });
 
+// GET A SINGLE BLOG
 const getSingleBlog = asyncHandler(async (req, res) => {
   const { id } = req.params;
   validateMongodbId(id);
@@ -40,12 +43,13 @@ const getSingleBlog = asyncHandler(async (req, res) => {
         new: true,
       }
     );
-    res.status(200).json(blog, updatedViews);
+    res.status(200).json(blog);
   } catch (error) {
     throw new Error(error);
   }
 });
 
+// GET ALL BLOGS
 const getAllBlogs = asyncHandler(async (req, res) => {
   try {
     const blogs = await Blog.find().sort({
@@ -59,6 +63,7 @@ const getAllBlogs = asyncHandler(async (req, res) => {
   }
 });
 
+// DELETE A BLOG
 const deleteBlog = asyncHandler(async (req, res) => {
   const { id } = req.params;
   validateMongodbId(id);
@@ -67,6 +72,21 @@ const deleteBlog = asyncHandler(async (req, res) => {
     if (!blog) return res.status(404).json({ message: "Blog not found!" });
 
     res.status(200).json({ message: "Blog deleted sucessfully!" });
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+// LIKE A BLOG
+const likeBlog = asyncHandler(async (req, res) => {
+  const { blogId } = req.body;
+  validateMongodbId(id);
+  try {
+    // Find the blog a user wants to like
+    const blog = await Blog.findById(blogId);
+    const userId = req.user._id;
+
+    const isLiked = blog.isLiked;
   } catch (error) {
     throw new Error(error);
   }
