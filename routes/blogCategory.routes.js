9454b7c4a056c63104cae1e9dfb293fedbc2aa57
IbroTao <ibrahimtaofeek005@gmail.com>
2024-01-11 08:35:44 +0100

    Blog Category Model, Controller & Route set up cont'd

--- routes/blogCategory.routes.js
+++ routes/blogCategory.routes.js
@@ -1,9 +1,18 @@
-const {Router} = require('express');
+const { Router } = require("express");
 const router = Router();
 
-router.post('/', authMiddleware, isAdmin, createBlog);
-router.get('/', authMiddleware, isAdmin, getCategories);
-router.get('/:id', authMiddleware, isAdmin, getCategory);
+const { authMiddleware, isAdmin } = require("../middlewares/authMiddleware");
+const {
+  createCategory,
+  getCategories,
+  getCategory,
+  updateCategory,
+  deleteCategory,
+} = require("../controllers/blogCategory.controller");
+
+router.post("/", authMiddleware, isAdmin, createCategory);
+router.get("/", authMiddleware, isAdmin, getCategories);
+router.get("/:id", authMiddleware, isAdmin, getCategory);
 router.put("/:id", authMiddleware, isAdmin, updateCategory);
 router.delete("/:id", authMiddleware, isAdmin, deleteCategory);
 
