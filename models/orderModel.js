59c306eff4f70a5cc6be6c032f50ef10b953ab70
IbroTao <ibrahimtaofeek005@gmail.com>
2024-01-11 17:24:29 +0100

    Order model set up cont'd

--- models/orderModel.js
+++ models/orderModel.js
@@ -1,31 +1,41 @@
 const mongoose = require("mongoose");
 
-const orderSchema = new mongoose.Schema({
-  products: [
-    {
-      product: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Products",
+const orderSchema = new mongoose.Schema(
+  {
+    products: [
+      {
+        product: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Products",
+        },
+        count: {
+          type: Number,
+        },
+        color: {
+          type: String,
+        },
       },
-      count: {
-        type: Number,
-      },
-      color: {
-        type: String,
-      },
-    },
-  ],
-  paymentIntent: {},
-  orderStatus: {
-    type: String,
-    default: "Not Processed",
-    enum: [
-      "Not Processed",
-      "Cash on Delivery",
-      "Processing",
-      "Dispatched",
-      "Cancelled",
     ],
+    paymentIntent: {},
+    orderStatus: {
+      type: String,
+      default: "Not Processed",
+      enum: [
+        "Not Processed",
+        "Cash on Delivery",
+        "Processing",
+        "Dispatched",
+        "Cancelled",
+      ],
+    },
+    orderedBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
   },
-  orderBy: {},
-});
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Order", OrderSchema);
