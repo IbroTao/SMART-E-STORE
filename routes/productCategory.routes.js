3a433e5a791359cef85a322853b432f0e9a21829
IbroTao <ibrahimtaofeek005@gmail.com>
2024-01-11 05:02:27 +0100

    Category model and route set up & also configured in index.js file

--- routes/productCategory.routes.js
+++ routes/productCategory.routes.js
@@ -4,11 +4,17 @@ const router = Router();
 const {
   createCategory,
   getAllCategories,
+  getSingleCategory,
+  updateCategory,
+  deleteCategory,
 } = require("../controllers/productCategory.controller");
 
 const { authMiddleware, isAdmin } = require("../middlewares/authMiddleware");
 
 router.post("/", authMiddleware, isAdmin, createCategory);
 router.get("/", authMiddleware, isAdmin, getAllCategories);
+router.get("/:id", authMiddleware, isAdmin, getSingleCategory);
+router.put("/:id", authMiddleware, isAdmin, updateCategory);
+router.delete("/:id".authMiddleware, isAdmin, deleteCategory);
 
 module.exports = router;
