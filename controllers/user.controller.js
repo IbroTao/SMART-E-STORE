9e042318d9b05a8f8df54751f21819fd29e2e29f
IbroTao <ibrahimtaofeek005@gmail.com>
2024-01-12 13:55:13 +0100

    Order set up

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -424,9 +424,26 @@ const emptyCart = asyncHandler(async (req, res) => {
 
 const applyCoupon = asyncHandler(async (req, res) => {
   const { coupon } = req.body;
+  const { _id } = req.user;
   try {
     const validCoupon = await Coupon.findOne({ name: coupon });
-    console.log(validCoupon);
+    if (validCoupon === null) {
+      throw new Error("Invalid Coupon");
+    }
+    const user = await User.findOne({ _id });
+    let { products, cartTotal } = await Cart.findOne({
+      orderBy: user._id,
+    }).populate("products.product");
+    let totalAfterDiscount = (
+      cartTotal -
+      (cartTotal * validCoupon.discount) / 100
+    ).toFixed(2);
+    await Cart.findOneAndUpdate(
+      { orderBy: user._id },
+      { totalAfterDiscount },
+      { new: true }
+    );
+    res.status(200).json(totalAfterDiscount);
   } catch (error) {
     throw new Error(error);
   }
